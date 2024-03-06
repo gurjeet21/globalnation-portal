f304748f44d96b575dc3e82ae8e35b5cfb5d9247
Amit Batra <86948970+amit-groupshop@users.noreply.github.com>
2024-03-06 17:30:08 +0530

    typo update

--- resources/views/pages/downloads.blade.php
+++ resources/views/pages/downloads.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <main class="flex flex-1 flex-col grow p-[1.875rem] overflow-y-auto">
-    <div class="breandcrumb-top">Pages | “Downloads</div>
+    <div class="breandcrumb-top">Pages | Downloads</div>
     @if(Session::has('success'))
         <div class="p-4 mb-2 mt-2 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
                 <span class="font-semibold">Success: </span> {{session('success')}}
